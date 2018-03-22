4a1ce1c6faa0896235551e4410723b8203a5e10e
Josh Landman <jlandman@wine.com>
2018-03-22 09:51:34 -0700

    Simple version
    * changed everything to modify clearly by reference
    * dropped the recursive descent parser
    * added script engine eval one-liner
    * killed the console log
    * added csv stdout
    * dropped the tuple wrappers
    * bumped file stats up to spreadsheet
    * dropped csvMap
    * declared the template where passed
    * removed the test seam returns
    * killed the test enum (wrong package anyway)
    * dropped that whole test - served it's purpose to show I unit tested each step - junit would replace
    * moved the program name output to stderr so you can run.sh 2>/dev/null

--- src/com/windfall/testapp/processors/MapToGrid.java
+++ src/com/windfall/testapp/processors/MapToGrid.java
@@ -1,10 +1,9 @@
 package com.windfall.testapp.processors;
 
 import java.util.Arrays;
+import java.util.Map;
 
-import com.windfall.testapp.models.CSVMap;
 import com.windfall.testapp.models.CellData;
-import com.windfall.testapp.models.FileStats;
 
 /**
  * convert CSVMap to Grid
@@ -15,8 +14,8 @@ public class MapToGrid {
 	
 	String[][] grid;
 
-	public MapToGrid(FileStats fs) {
-		grid = new String[fs.rowCount][fs.maxFieldsInRow];
+	public void initGrid(int row, int col) {
+		grid = new String[row][col];
 	}
 
 	/** 
@@ -24,8 +23,8 @@ public class MapToGrid {
 	 * @param csvMap celldata
 	 * @return string array - uninit will NPE
 	 */
-	public String[][] mapToGrid(CSVMap csvMap) {
-		csvMap.getCsvMap().entrySet().stream().forEach( e-> {
+	public String[][] mapToGrid(Map<String,CellData> csvMap) {
+		csvMap.entrySet().stream().forEach( e-> {
 			CellData cd = e.getValue();
 			grid[cd.r-1][cd.c-1] = String.format("%.2f", (cd.calculated) ? cd.evaluatedValue:0) ;
 				 //String.format("%.2f", cd.evaluatedValue) : cd.text; //to output text when not eval'd
